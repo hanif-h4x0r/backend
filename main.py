341dd2a833def834cb2ce4d0f5657d0fe8b7a05b
hanif-130506 <hanifabdurrahmanarifai@gmail.com>
2026-06-07 17:52:50 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
-from fastapi import FastAPI
-from pydantic import BaseModel # This import was for build data schema
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
-# Initialization fastapi application
+# Import connection database and table that we made in another file
+import models
+from database import engine, get_db
+
 app = FastAPI()
 
-# Make schema data product with pydantic
+# Request to SQLAlchemy to automate create table product in Neon DB
+models.Base.metadata.create_all(bind=engine)
 
-class Product(BaseModel):
-    name: str
+# Schema pydantic for validation
+class SchemaProduct(BaseModel):
+    name: str 
     price: int
-    is_ready: bool = True # default was true if user didn't input
+    is_ready: bool = True
 
-database_product = [] #Temporary database
-
+# endpoint get to get all of product in neon databse
 @app.get("/product")
-def get_all_product():
-    return {"total_product": len(database_product), "data": database_product}
+def get_all_product(db: Session = Depends(get_db)):
+    # Query sql ORM: SELECT * FROM product
+    all_product = db.query(models.ModelProduct).all()
+    return {"total_product": len(all_product), "data": all_product}
 
-@app.get('/')
-def home():
-    return {"message": "This is my first API"}
+# endpoint post to post new product to neon database
 
-
-@app.post("/add-product")
-def add_product(new_items: Product): # use schema product that we made
-    # change data fromo pytdantic become ordinary dictionary, then save to our mini database
-    product_dict = new_items.model_dump()
-    database_product.append(product_dict)
-
-    return {"message": "Product added successfully", "data_recorded": product_dict}
+@app.post("/product/add-product")
+def add_product(new_product: SchemaProduct, db: Session = Depends(get_db)):
+    # Change model from pydantic become model database SQLAlchemy
+    product_db = models.ModelProduct{}
