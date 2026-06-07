e4209b034edd29463c8fdb0accc9f2b32f2d9f40
hanif-130506 <hanifabdurrahmanarifai@gmail.com>
2026-06-07 15:44:44 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel # This import was for build data schema
 
 # Initialization fastapi application
 app = FastAPI()
 
-# Make endpoint, method(GET, Path: "/")
-@app.get("/")
-def home():
-    return {"message": "Hello world, this is my first API!"}
+# Make schema data product with pydantic
 
-# Create a second endpoint for looking at user data based on user_id
-@app.get("/user/{user_id}")
-def get_user(user_id: int):
-    # example collect data from database
-    return {
-        "user_id": user_id,
-        "name": "Hanif",
-        "status": "Active"
-    }
+class Product(BaseModel):
+    name: str
+    price: int
+    is_ready: bool = True # default was true if user didn't input
+
+database_product = [] #Temporary database
+
+@app.get("/product")
+def get_all_product():
+    return {"total_product": len(database_product), "data": database_product}
+
+@app.get('/')
+def home():
+    return {"message": "This is my first API"}
+
+
+@app.post("/add-product")
+def add_product(new_items: Product): # use schema product that we made
+    # change data fromo pytdantic become ordinary dictionary, then save to our mini database
+    product_dict = new_items.model_dump()
+    database_product.append(product_dict)
+
+    return {"message": "Product added successfully", "data_recorded": product_dict}
